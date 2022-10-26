34ff660aa6489cea375b74ba583ddc56bb717e26
Kai Vandivier <kpvandivier@gmail.com>
2022-10-26 16:15:03 +0200

    feat: add file upload button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { DataQuery } from "@dhis2/app-runtime";
 import i18n from "@dhis2/d2-i18n";
 import React from "react";
 import classes from "./App.module.css";
+import UploadPackage from "./components/upload-package/upload-package.js";
+import "./locales/index.js";
 
 const query = {
     me: {
@@ -11,24 +13,7 @@ const query = {
 
 const MyApp = () => (
     <div className={classes.container}>
-        <DataQuery query={query}>
-            {({ error, loading, data }) => {
-                if (error) {
-                    return <span>ERROR</span>;
-                }
-                if (loading) {
-                    return <span>...</span>;
-                }
-                return (
-                    <>
-                        <h1>
-                            {i18n.t("Hello {{name}}", { name: data.me.name })}
-                        </h1>
-                        <h3>{i18n.t("Welcome to DHIS2!")}</h3>
-                    </>
-                );
-            }}
-        </DataQuery>
+        <UploadPackage />
     </div>
 );
 
