189d30115a1a304feed29b82e240770e0ed1ed24
Thomas Zemp <tom@dhis2.org>
2022-10-26 16:49:58 +0200

    chore: set up file upload

--- src/components/Inspect/Inspect.js
+++ src/components/Inspect/Inspect.js
@@ -25,13 +25,15 @@ const query = {
     },
 };
 
-export const Inspect = ({ goToNextStep }) => {
+export const Inspect = ({ goToNextStep, metadataPackage }) => {
     const { data } = useDataQuery(query);
     const { systemInfo } = useConfig();
 
     if (data && systemInfo) {
         return (
             <div>
+                <span>{JSON.stringify(metadataPackage)}</span>
+                <br />
                 <span>Wrong DHIS2 version</span>
                 <br />
                 <span>Default disaggregation is different </span>
@@ -52,4 +54,5 @@ export const Inspect = ({ goToNextStep }) => {
 
 Inspect.propTypes = {
     goToNextStep: PropTypes.func,
+    metadataPackage: PropTypes.object,
 };
