db96c6078b0ff2989feadb01288c9c4078459b7f
Thomas Zemp <tom@dhis2.org>
2022-10-26 16:15:51 +0200

    chore: expand shell

--- src/Inspect/Inspect.js
+++ src/Inspect/Inspect.js
@@ -1,3 +1,56 @@
+import { useDataQuery, useConfig } from "@dhis2/app-runtime";
+import {Button} from '@dhis2/ui'
+import PropTypes from 'prop-types'
 import React from "react";
 
-export const Inspect = () => <span>Inspect step goes here</span>;
+
+const query = {
+    categoryCombos: {
+        resource: "categoryCombos",
+        params: {
+            paging: false,
+            filter: "name:eq:default",
+        },
+    },
+    trackedEntityTypes: {
+        resource: "trackedEntityTypes",
+        params: {
+            paging: false,
+        },
+    },
+    indicatorTypes: {
+        resource: "indicatorTypes",
+        params: {
+            paging: false,
+        },
+    },
+};
+
+export const Inspect = ({goToNextStep}) => {
+    const { data } = useDataQuery(query);
+    const { systemInfo } = useConfig();
+
+    if (data && systemInfo) {
+        return (
+            <div>
+                <span>Wrong DHIS2 version</span>
+                <br />
+                <span>Default disaggregation is different </span>
+                <br />
+                <span>
+                    Do you want to map the tracked entity type to an existing
+                    one?{" "}
+                </span>
+                <br />
+                <span>Map indicator types</span>
+                <br />
+                <Button onClick={goToNextStep}>Go to dry run</Button>
+            </div>
+        );
+    }
+    return null;
+};
+
+Inspect.propTypes = {
+    goToNextStep: PropTypes.func,
+}
